ae869883e23b04650b5defbc528ae3ff81e9d3ef
sniper365 <devtops2050@gmail.com>
2021-07-28 16:17:57 +0100

    install repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo, ReactNode } from 'react'
 import ReactDOM from 'react-dom'
+import { stopReportingRuntimeErrors } from "react-error-overlay";
 import useActiveWeb3React from './hooks/useActiveWeb3React'
 import { BLOCKED_ADDRESSES } from './config/constants'
 import ApplicationUpdater from './state/application/updater'
@@ -8,6 +9,10 @@ import MulticallUpdater from './state/multicall/updater'
 import TransactionUpdater from './state/transactions/updater'
 import App from './App'
 import Providers from './Providers'
+
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
 
 function Updaters() {
   return (
