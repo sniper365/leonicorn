9e8282cd02fa4322f883f1bdb9e6c722fcbf1cbd
Sintaklaas <sklaas034@gmail.com>
2021-08-06 14:31:48 +0200

    Changed background-color of language-picker and telegram groups popup.

--- src/toolkit/uikit/components/Dropdown/Dropdown.tsx
+++ src/toolkit/uikit/components/Dropdown/Dropdown.tsx
@@ -24,7 +24,7 @@ const DropdownContent = styled.div<{ position: Position }>`
   transform: translate(-50%, 0);
   left: ${getLeft};
   bottom: ${getBottom};
-  background-color: ${({ theme }) => theme.nav.background};
+  background-color: #303742;
   box-shadow: ${({ theme }) => theme.shadows.level1};
   padding: 16px;
   max-height: 400px;
